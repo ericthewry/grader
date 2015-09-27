9df213b18abc74c03be0eeb434a0f54a47152181
Eric Campbell <1234goal@gmail.com>
2015-09-26 21:38:35 -0700

    add flags

--- grader.py
+++ grader.py
@@ -2,13 +2,14 @@ from argparse import ArgumentParser
 
 import re
 import time
+import getpass
 
 def main():
      parser = ArgumentParser("""The command line tool for grading""")
      parser.add_argument('command')
-     parser.add_argument('--send')
-     parser.add_argument('--test')
-     parser.add_argument('--refresh')
+     parser.add_argument('--send', action='store_true')
+     parser.add_argument('--test', action='store_true')
+     parser.add_argument('--refresh', action='store_true')
 
      args = parser.parse_args()
 
@@ -18,29 +19,37 @@ def main():
           i, j = re.search('[0123456789]+', course).span()
           courseNum = int(course[i:j])
           print("Welcome to the Grading environment for CS%d ..." % courseNum)
+
      elif args.command == "make-pdfs":
           source = input("Please specify the input directory: ")
           dest   = input("Please specify the output directory: ")
+
           print("making your pdfs...")
           time.sleep(2)
           print("Done!")
-     elif args.command == "send-emails":
-          emails   = input("Please specify the path to emails.csv: ")
-          gradeDir = input("Please specify the directory of the Grade Files:")
-          print("sending your emails...")
-          time.sleep(4)
-          print("Done!")
-     elif args.command == "test-emails":
-          emails   = input("Please specify the path to emails.csv: ")
-          gradeDir = input("Please specify the directory of the Grade Files:")
-          print("linting grade submissions...")
-          time.sleep(4)
-          print("Done!")
-     elif args.command == "refresh-emails":
-          emails   = input("Please specify the path to emails.csv: ")
-          print("refreshing sent emails")
-          time.sleep(1)
-          print("Done!")
+     elif args.command == "emails":
+          if args.send:
+               emails   = input("Please specify the path to emails.csv: ")
+               gradeDir = input("Please specify the directory of the Grade Files:")
+               sender   = input("Which email would you like to use? ")
+               password = getpass.getpass("What is your password? ")
+
+               print("sending your emails...")
+               time.sleep(4)
+               print("Done!")
+          elif args.test:
+               emails   = input("Please specify the path to emails.csv: ")
+               gradeDir = input("Please specify the directory of the Grade Files:")
+
+               print("linting grade submissions...")
+               time.sleep(4)
+               print("Done!")
+          elif args.refresh:
+               emails   = input("Please specify the path to emails.csv: ")
+
+               print("refreshing sent emails")
+               time.sleep(1)
+               print("Done!")
      else:
           print("unrecognized command")
 
